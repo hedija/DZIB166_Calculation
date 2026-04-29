882ff3452b7e43d7e92e2da9c4feadb9fc794fc2
Hedija <hedija@gmail.com>
2026-04-29 23:13:38 +0300

    merge

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/DZIB166_calculation/',
   plugins: [react()],
   define: {
     'process.env.NODE_ENV': JSON.stringify('production'),
